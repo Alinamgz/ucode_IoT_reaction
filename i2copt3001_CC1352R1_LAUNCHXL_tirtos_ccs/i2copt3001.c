6b238192f2d65b5be74877c6dffb13af032414a0
Alina Mazur <amazur@e1r9p8.unit.ua>
2021-07-12 22:25:34 +0300

    finished. have to add readme

--- i2copt3001_CC1352R1_LAUNCHXL_tirtos_ccs/i2copt3001.c
+++ i2copt3001_CC1352R1_LAUNCHXL_tirtos_ccs/i2copt3001.c
@@ -47,24 +47,44 @@ static OPT3001_Params opt3001Params;
 
 s32 uncomp_temperature   = 0;
 
-float lux;
-float comp_temperature;
+float raw_lux;
+float raw_temperature;
+
+float processed_lux;
+float processed_temperature;
+
+//uint8_t cur_value;
+
+enum tresholds{
+    min_lux = 15,
+    max_lux = 25,
+    min_tmp = 30,
+    max_tmp = 34
+};
 
 /***** Functions *****/
 extern s32 bme280_data_readout_template(I2C_Handle i2cHndl);
+
+static inline void say_error(char *where_err);
 static inline void config_display(void);
 static inline void config_I2C(void);
 static inline void config_sensors(void);
+
+static float moving_avg_filter(float raw_value, float *buf, uint8_t buf_len);
+static void check_filtered_data(float cur_lux, float cur_tmp);
 
 /*
  *  ======== mainThread ========
  */
 void *mainThread(void *arg0) {
+
+    float buf_lux_filter[BUF_LEN] = {0};
+    float buf_tmp_filter[BUF_LEN] = {0};
+
     /* Call driver init functions */
     GPIO_init();
     I2C_init();
     OPT3001_init();
-
 
     config_display();
     config_I2C();
@@ -76,25 +96,43 @@ void *mainThread(void *arg0) {
     while(1) {
 
         /* Get Lux */
-        if (!OPT3001_getLux(opt3001Handle, &lux)) {
-            Display_print0(display, 0, 0, "OPT3001 sensor read failed");
+        if (!OPT3001_getLux(opt3001Handle, &raw_lux)) {
+            say_error("Reading from the OPT3001");
         }
 
-        Display_print1(display, 0, 0, "Lux: %f\n\n", lux);
+        processed_lux = moving_avg_filter(raw_lux, buf_lux_filter, BUF_LEN);
+        Display_printf(display, 0, 0, "\nRAW Lux: %f    \tPRC Lux: %f", raw_lux, processed_lux);
 
         if (BME280_INIT_VALUE == bme280_read_uncomp_temperature(&uncomp_temperature)) {
-            comp_temperature = (float)bme280_compensate_temperature_double(uncomp_temperature);
+            raw_temperature = (float)bme280_compensate_temperature_double(uncomp_temperature);
         }
         else {
-            Display_print0(display, 0, 0, "Error reading from the bme280 sensor\n");
+            say_error("Reading from the bme280");
         }
 
-//               sleep(1);
+        processed_temperature = moving_avg_filter(raw_temperature, buf_tmp_filter, BUF_LEN);
+        Display_printf(display, 0, 0, "RAW tmp: %f    \tPRC tmp: %f", raw_temperature, processed_temperature);
+
+        check_filtered_data(processed_lux, processed_temperature);
+
         sleep(SAMPLE_TIME);
     }
 }
 
 //==================================================================================
+//==================================================================================
+
+//================================ ERRORS ==========================================
+static inline void say_error(char *where_err) {
+    Display_printf(display, 0, 0, "!!!\t%s ERR !!!\n\r");
+
+    while (1) {
+        GPIO_toggle(CONFIG_GPIO_LED_Red);
+        usleep(25000);
+    }
+}
+
+
 //================================ DISPLAY =========================================
 
 static inline void config_display(void) {
@@ -106,9 +144,6 @@ static inline void config_display(void) {
             usleep(25000);
         }
     }
-    else {
-        Display_printf(display, 0, 0, "...\n\r");
-    }
 }
 
 //================================== I2C ============================================
@@ -118,11 +153,7 @@ static inline void config_I2C(void) {
     i2cHandle = I2C_open(CONFIG_I2C_SENSORS, &i2cParams);
 
     if (i2cHandle == NULL) {
-        Display_print0(display, 0, 0, "Error Initializing I2C\n");
-        while (1) {
-            GPIO_toggle(CONFIG_GPIO_LED_Red);
-            usleep(25000);
-        }
+        say_error("Initializing I2C");
     }
     else {
         Display_print0(display, 0, 0, "I2C Initialized!\n");
@@ -137,18 +168,54 @@ static inline void config_sensors(void) {
     opt3001Handle = OPT3001_open(CONFIG_OPT3001_LIGHT, i2cHandle, &opt3001Params);
 
     if(opt3001Handle == NULL) {
-        Display_print0(display, 0, 0, "OPT3001 Open Failed!");
-        while(1) {
-            GPIO_toggle(CONFIG_GPIO_LED_Red);
-            usleep(25000);
-        }
+        say_error("OPT3001 Open");
     }
 
     sleep(1);
 
 /* Initialize the BME Sensor */
     if(BME280_INIT_VALUE != bme280_data_readout_template(i2cHandle)) {
-        Display_print0(display, 0, 0, "Error Initializing bme280\n");
+        say_error("Initializing bme280");
     }
+
     bme280_set_power_mode(BME280_NORMAL_MODE);
+}
+
+//==================================================================================
+//==================================================================================
+//================================ FILTER ==========================================
+static float moving_avg_filter(float raw_value, float *buf, uint8_t buf_len) {
+    uint8_t i = 0;
+    float rslt_value = 0;
+
+    buf[buf_len - 1] = raw_value;
+
+    for (i = 0; i < buf_len; i++) {
+        rslt_value += buf[i];
+        buf[i] = (1 + i) < buf_len ? buf[1 + i] : buf[i];
+    }
+
+    rslt_value = rslt_value / buf_len;
+
+    return rslt_value;
+}
+
+//======================== CHECK value & SWITCH LED ==================================
+static void check_filtered_data(float cur_lux, float cur_tmp) {
+    if (cur_lux >= min_lux && cur_lux <= max_lux) {
+        GPIO_write(CONFIG_GPIO_LED_Green, CONFIG_GPIO_LED_ON);
+        Display_printf(display, 0, 0, "------    \tLux hobba!\t------\n\n\r");
+    }
+    else {
+        GPIO_write(CONFIG_GPIO_LED_Green, CONFIG_GPIO_LED_OFF);
+    }
+
+    if (cur_tmp <= max_tmp && cur_tmp >= min_tmp) {
+        Display_printf(display, 0, 0, "------\tTemperature hobba!\t------\n\n\r");
+        GPIO_write(CONFIG_GPIO_LED_Red, CONFIG_GPIO_LED_ON);
+    }
+    else {
+        GPIO_write(CONFIG_GPIO_LED_Red, CONFIG_GPIO_LED_OFF);
+    }
+
 }
